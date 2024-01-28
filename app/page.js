f309366a3a23b78a0028dd3900d23628cdff68b0
fak3mistak3 <mirolyubov.dev@gmail.com>
2024-01-28 17:51:39 +0400

    update url

--- app/page.js
+++ app/page.js
@@ -6,8 +6,8 @@ export default function Home() {
     return (
         <main className={styles.main}>
             <Player
-                // src="http://188.166.97.181:8888/test/index.m3u8"
-                url="http://localhost:8888/test/index.m3u8"
+                src="http://188.166.97.181:8888/test/index.m3u8"
+                // url="http://localhost:8888/test/index.m3u8"
             />
         </main>
     );
